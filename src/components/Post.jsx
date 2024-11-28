f4cd22c07e7f3309490e0b6bffa0b400a172f331
SimoneChiodo <simochi.005@gmail.com>
2024-11-28 16:19:33 +0100

    add Header, Main, Footer, Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,24 @@
+export default function () {
+    return (
+        <div className="card">
+            <img
+                src="https://kentkonseyi.karatay.bel.tr/images/placeholder/600x400.jpg"
+                className="card-img-top"
+                alt="Post-Image"
+            ></img>
+            <div className="card-body">
+                <h5 className="card-title">Titolo del post</h5>
+                <p className="card-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Omnis blanditiis debitis adipisci incidunt architecto est
+                    necessitatibus expedita excepturi iure accusamus soluta
+                    facilis totam ullam maiores optio illo, consequuntur nisi
+                    ad.
+                </p>
+                <a href="#" className="btn btn-warning">
+                    Leggi di più
+                </a>
+            </div>
+        </div>
+    );
+}
